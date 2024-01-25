e1aa3efdd9c09f0730f0dcc3948188108ed97754
Andrey Melnikov <113206813+aeksin@users.noreply.github.com>
2024-01-25 21:10:13 +0300

    pr1

---
+++ 1-17.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ $# == 0 ]]
+then 
+	echo "please select the file or run script with -h(--help) flag"
+elif [[ $1 == "-h" || $1 == "--help" ]]
+then
+	echo "this program output even lines of file to out_even.txt, odd to out_odd.txt, and copy of the file to out_orig.txt"
+	echo "to run program, input path to file as argument"
+	exit 0
+else
+	if [ -f $1 ] 
+	then 
+		sed -n 2~2p $1 > out_even.txt
+		sed -n 1~2p $1 > out_odd.txt
+		sed -n 1~1p $1 > out_orig.txt
+		exit 0
+	else
+		echo "couldn't open the file"
+		exit 1
+	fi 
+fi
